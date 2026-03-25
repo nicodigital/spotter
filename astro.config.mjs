f408e087750cce18e7a3debc35b42f4c1920a74d
Nicolás González <nicolook.pro@gmail.com>
2026-03-25 17:41:46 -0300

    Netlify Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
