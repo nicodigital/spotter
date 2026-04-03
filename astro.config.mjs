66e8990b62005e4a25d5829fd907e550c5b9a8ad
Nicolás González <nicolook.pro@gmail.com>
2026-04-03 15:44:05 -0300

    Deploy en Cloudflare Workers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
+
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
