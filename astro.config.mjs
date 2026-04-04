f6875ce00103ee5aed049f055a5fd1ea8a4b06e3
Nicolás González <nicolook.pro@gmail.com>
2026-04-04 13:30:55 -0300

    New Deploy on Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-import vercel from '@astrojs/vercel';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
